c75ade55def708b4ba494b0a2a6fcb7f3fc12b8a
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 15:23:01 +0200

    Update ModalSignUp.js

--- src/components/Modal/ModalSignUp.js
+++ src/components/Modal/ModalSignUp.js
@@ -25,6 +25,13 @@ class ModalSignUp extends Component {
 
         <Modal show={this.state.isOpen}
           onClose={this.toggleModal}>
+            <div>
+              <h3>Welcome to Smoke.Network Alpha</h3>
+              <p>Smoke.Network Alpha runs on Steem blockchain allowing our authors to earn real cryptocurrency that can be traded for bitcoin and other altcoins.</p>
+              <p>Due to the nature of the steem blockchain you must first sign up for an account on Steemit.com, the official application of the Steem blockchain.
+                 <br />
+                 Once you have a Steem account you can sign in here and enjoy all the benefits Smoke.Network has to provide such as a vibrant community of stoners, a boosting bot that upvotes any posts posted from out platform using the #smokenetwork tag.</p> 
+            </div>
             <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
               <button className="Modal__button">
                 <FormattedMessage id="signup" defaultMessage="Sign up" />
