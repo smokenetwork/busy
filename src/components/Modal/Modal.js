5bb05854d941ac50da86240080621655ae9b76b2
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 14:28:20 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Modal.less';
 
 class Modal extends React.Component {
   render() {
@@ -8,30 +9,9 @@ class Modal extends React.Component {
       return null;
     }
 
-    // The gray background
-    const backdropStyle = {
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'rgba(0,0,0,0.3)',
-      padding: 50
-    };
-
-    // The modal "window"
-    const modalStyle = {
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      maxWidth: 500,
-      minHeight: 300,
-      margin: '0 auto',
-      padding: 30
-    };
-
     return (
-      <div className="backdrop" style={backdropStyle}>
-        <div className="modal" style={modalStyle}>
+      <div className="backdropStyle">
+        <div className="modalStyle">
           {this.props.children}
 
           <div className="footer">
