049db833900be7e1206e256ca08bd1e21fb39aa5
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 19:54:50 +0200

    Update SignUp.js

--- src/components/Sidebar/SignUp.js
+++ src/components/Sidebar/SignUp.js
@@ -8,19 +8,19 @@ const SignUp = () =>
     <h3 className="SidebarBlock__title">
       <FormattedMessage id="new_to_smoke" defaultMessage="New to Smoke.Network?" />
     </h3>
-    <div className="Step1">
+    <div className="Steps Step1">
       <i className="iconfont icon-success_fill SidebarBlock__steps" />
       <FormattedMessage id="step_1" defaultMessage="Sign up to Steem" />
     </div>
-    <div className="Step2">
+    <div className="Steps Step2">
       <i className="iconfont icon-success_fill SidebarBlock__steps" />
       <FormattedMessage id="step_2" defaultMessage="Wait for confirmation" />
     </div>
-    <div className="Step3">
+    <div className="Steps Step3">
       <i className="iconfont icon-success_fill SidebarBlock__steps" />
       <FormattedMessage id="step_3" defaultMessage="Grab your posting key" />
     </div>
-    <div className="Step4">
+    <div className="Steps Step4">
       <i className="iconfont icon-success_fill SidebarBlock__steps" />
       <FormattedMessage id="step_4" defaultMessage="Log in" />
     </div>
