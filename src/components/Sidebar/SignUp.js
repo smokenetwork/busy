d4d0c9616b94d4feb762dbba5e20cd7be35bbdb4
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 17:03:41 +0200

    Update SignUp.js

--- src/components/Sidebar/SignUp.js
+++ src/components/Sidebar/SignUp.js
@@ -8,6 +8,12 @@ const SignUp = () =>
     <h3 className="SidebarBlock__title">
       <FormattedMessage id="new_to_smoke" defaultMessage="New to SmokeNetwork?" />
     </h3>
+    <div>
+      <i className="iconfont icon-group InterestingPeople__icon" />
+      </i>
+      Test
+    </div>
+    
     <ModalSignUp />
       
   </div>);
