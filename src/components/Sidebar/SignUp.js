607928762fb8ec4559c2e0d03e8497da47f8968f
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 12:35:12 +0200

    Update SignUp.js

--- src/components/Sidebar/SignUp.js
+++ src/components/Sidebar/SignUp.js
@@ -1,46 +1,20 @@
 import React from 'react';
-import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import './SidebarBlock.less';
-import Modal from '../Modal/Modal';
+import 'ModalApp' from '../Modal/ModalApp';
 
-class SignUp extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { isOpen: false };
-  }
-
-  toggleModal = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.toggleModal}>
-          Open the modal
+const SignUp = () =>
+  (<div className="SidebarBlock">
+    <h3 className="SidebarBlock__title">
+      <FormattedMessage id="new_to_smoke" defaultMessage="New to SmokeNetwork?" />
+    </h3>
+    <ModalApp>
+      <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
+        <button className="SidebarBlock__button">
+          <FormattedMessage id="signup" defaultMessage="Sign up" />
         </button>
-
-        <Modal show={this.state.isOpen}
-          onClose={this.toggleModal}>
-          <div className="SidebarBlock">
-            <h3 className="SidebarBlock__title">
-               <FormattedMessage id="new_to_smoke" defaultMessage="New to SmokeNetwork?" />
-            </h3>
-            <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
-              <button className="SidebarBlock__button">
-               <FormattedMessage id="signup" defaultMessage="Sign up" />
-              </button>
-            </a>
-          </div>
-        </Modal>
-      </div>
-    );
-  }
-}
-
+      </a>
+    <ModalApp />
+  </div>);
 
 export default SignUp;
