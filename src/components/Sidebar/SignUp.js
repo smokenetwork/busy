4f98c5fe91256e4ce32eaf0af0c5fab8c42e9894
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 19:19:32 +0200

    Update SignUp.js

--- src/components/Sidebar/SignUp.js
+++ src/components/Sidebar/SignUp.js
@@ -6,12 +6,23 @@ import ModalSignUp from '../Modal/ModalSignUp'
 const SignUp = () =>
   (<div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
-      <FormattedMessage id="new_to_smoke" defaultMessage="New to SmokeNetwork?" />
+      <FormattedMessage id="new_to_smoke" defaultMessage="New to Smoke.Network?" />
     </h3>
-    <div>
-      <i className="iconfont icon-group InterestingPeople__icon" />
-      
-      Test
+    <div className="Step1">
+      <i className="iconfont icon-success_fill" />
+      <FormattedMessage id="step_1" defaultMessage="Sign up to Steem" />
+    </div>
+    <div className="Step2">
+      <i className="iconfont icon-success_fill" />
+      <FormattedMessage id="step_2" defaultMessage="Wait for confirmation" />
+    </div>
+    <div className="Step3">
+      <i className="iconfont icon-success_fill" />
+      <FormattedMessage id="step_3" defaultMessage="Grab your posting key" />
+    </div>
+    <div className="Step4">
+      <i className="iconfont icon-success_fill" />
+      <FormattedMessage id="step_4" defaultMessage="Login to Smoke.Network" />
     </div>
     
     <ModalSignUp />
