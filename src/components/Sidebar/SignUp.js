fc1eed8e54a1dba7a07a300c354336119635eae0
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 12:58:08 +0200

    Update SignUp.js

--- src/components/Sidebar/SignUp.js
+++ src/components/Sidebar/SignUp.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import './SidebarBlock.less';
+import ModalApp from '../Modal/ModalApp'
 
 const SignUp = () =>
   (<div className="SidebarBlock">
     <h3 className="SidebarBlock__title">
       <FormattedMessage id="new_to_smoke" defaultMessage="New to SmokeNetwork?" />
     </h3>
+    <ModalApp />
       <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
         <button className="SidebarBlock__button">
           <FormattedMessage id="signup" defaultMessage="Sign up" />
