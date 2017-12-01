a45f7945ac32f258fcc9ddca86b77197ab51f002
Jonathan Hunter <info@lostsouth.co.za>
2017-12-01 13:27:08 +0200

    Update ModalSignUp.js

--- src/components/Modal/ModalSignUp.js
+++ src/components/Modal/ModalSignUp.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Modal from './Modal';
 import { FormattedMessage } from 'react-intl';
-import './Sidebar/SidebarBlock.less';
 
 class ModalSignUp extends Component {
   constructor(props) {
@@ -25,11 +24,11 @@ class ModalSignUp extends Component {
 
         <Modal show={this.state.isOpen}
           onClose={this.toggleModal}>
-          <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
-            <button className="SidebarBlock__button">
-            <FormattedMessage id="signup" defaultMessage="Sign up" />
-            </button>
-          </a>
+            <a target="_blank" rel="noopener noreferrer" href="https://steemit.com/pick_account">
+              <button>
+                <FormattedMessage id="signup" defaultMessage="Sign up" />
+              </button>
+            </a>
         </Modal>
       </div>
     );
